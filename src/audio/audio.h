e45ef2649bbd70fc0857d4ab8c16b2c30fe1d49a
Eknous-P <ehovakimyan053@gmail.com>
2024-04-25 19:20:30 +0400

    cmakelists and audio class stuf

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -23,6 +23,13 @@ class AudioInput {
 
 	public:
 
+		static int bufferWriteCallback(void* inputBuffer,
+																	 void* outputBuffer,
+																	 unsigned long framesPerBuffer,
+        													 const PaStreamCallbackTimeInfo* timeInfo,
+        													 PaStreamCallbackFlags statusFlags,
+        													 void *userData );
+
 		AudioInput();
 		~AudioInput();
 };
