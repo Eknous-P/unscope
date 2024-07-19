5e43de9acce68e9de098632304bf622d73af5467
Eknous-P <ehovakimyan053@gmail.com>
2024-07-20 01:49:24 +0400

    disable unnecessary stuff as if it makes a difference

--- src/gui/render/render_opengl_sdl.cpp
+++ src/gui/render/render_opengl_sdl.cpp
@@ -2,7 +2,7 @@
 
 int USCRendOGL_SDL::setup() {
   // Setup SDL
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
     printf("Error: %s\n", SDL_GetError());
     return -1;
   }
