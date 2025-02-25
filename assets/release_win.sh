bbd308034824e1eee3dfda8f88502526865b91bd
Eknous-P <ehovakimyan053@gmail.com>
2025-02-25 21:49:56 +0400

    fix release script

--- assets/release_win.sh
+++ assets/release_win.sh
@@ -7,4 +7,5 @@ cmake .. --toolchain ../tc_mingw.cmake -DCMAKE_BUILD_TYPE='Release'
 make -j8
 cp ../LICENSE ./LICENSE
 cp ../README.md ./README.md
-zip ./unscope_win64.zip ./unscope.exe ./LICENSE ./README.md ./submodules/SDL/SDL2.dll
+cp ./submodules/SDL/SDL2.dll .
+zip ./unscope_win64.zip ./unscope.exe ./LICENSE ./README.md ./SDL2.dll
