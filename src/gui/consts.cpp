5112c3b95942fdc157acf7f34e466dd32be06fd4
Eknous-P <ehovakimyan053@gmail.com>
2025-10-16 15:29:54 +0400

    update layout

--- src/gui/consts.cpp
+++ src/gui/consts.cpp
@@ -35,46 +35,46 @@ Collapsed=0\n\
 DockId=0x00000002,0\n\
 \n\
 [Window][Scope]\n\
-Pos=0,180\n\
-Size=988,309\n\
+Pos=0,179\n\
+Size=988,310\n\
 Collapsed=0\n\
-DockId=0x00000009,0\n\
+DockId=0x0000000E,0\n\
 \n\
 [Window][Scope (XY)]\n\
-Pos=990,180\n\
-Size=290,309\n\
+Pos=990,179\n\
+Size=290,310\n\
 Collapsed=0\n\
-DockId=0x0000000A,0\n\
+DockId=0x0000000C,0\n\
 \n\
 [Window][Global Controls]\n\
 Pos=990,491\n\
 Size=290,229\n\
 Collapsed=0\n\
-DockId=0x00000008,0\n\
+DockId=0x00000004,0\n\
 \n\
 [Window][Cursors]\n\
 Pos=990,491\n\
 Size=290,229\n\
 Collapsed=0\n\
-DockId=0x00000008,1\n\
+DockId=0x00000004,1\n\
 \n\
 [Window][Channel 1 Controls]\n\
 Pos=0,491\n\
-Size=493,229\n\
-Collapsed=0\n\
-DockId=0x00000005,0\n\
-\n\
-[Window][Channel 2 Controls]\n\
-Pos=504,491\n\
-Size=493,229\n\
+Size=494,229\n\
 Collapsed=0\n\
 DockId=0x00000007,0\n\
 \n\
-[Window][XY Scope Controls]\n\
-Pos=990,180\n\
-Size=290,309\n\
+[Window][Channel 2 Controls]\n\
+Pos=496,491\n\
+Size=492,229\n\
 Collapsed=0\n\
-DockId=0x0000000A,1\n\
+DockId=0x00000008,0\n\
+\n\
+[Window][XY Scope Controls]\n\
+Pos=990,179\n\
+Size=290,310\n\
+Collapsed=0\n\
+DockId=0x0000000C,1\n\
 \n\
 [Window][WindowOverViewport_11111111]\n\
 Pos=0,19\n\
@@ -83,13 +83,13 @@ Collapsed=0\n\
 \n\
 [Window][Spectrum Controls]\n\
 Pos=990,19\n\
-Size=290,159\n\
+Size=290,158\n\
 Collapsed=0\n\
-DockId=0x0000000E,0\n\
+DockId=0x0000000B,0\n\
 \n\
 [Window][Spectrum]\n\
 Pos=0,19\n\
-Size=988,159\n\
+Size=988,158\n\
 Collapsed=0\n\
 DockId=0x0000000D,0\n\
 \n\
@@ -104,22 +104,22 @@ Size=806,662\n\
 Collapsed=0\n\
 \n\
 [Docking][Data]\n\
-DockSpace         ID=0x8B93E3BD Pos=0,19 Size=1280,701 Split=X Selected=0x7C3EDFF1\n\
-  DockNode        ID=0x00000001 Parent=0x8B93E3BD SizeRef=578,371 CentralNode=1 Selected=0x7C3EDFF1\n\
-  DockNode        ID=0x00000002 Parent=0x8B93E3BD SizeRef=275,371 HiddenTabBar=1 Selected=0x67284010\n\
-DockSpace         ID=0xD6C4A87D Window=0x1BBC0F80 Pos=0,19 Size=1280,701 Split=Y Selected=0xB159AA79\n\
-  DockNode        ID=0x0000000B Parent=0xD6C4A87D SizeRef=1280,159 Split=X Selected=0x376897DD\n\
-    DockNode      ID=0x0000000D Parent=0x0000000B SizeRef=988,165 Selected=0x376897DD\n\
-    DockNode      ID=0x0000000E Parent=0x0000000B SizeRef=290,165 Selected=0x839614CF\n\
-  DockNode        ID=0x0000000C Parent=0xD6C4A87D SizeRef=1280,540 Split=Y\n\
-    DockNode      ID=0x00000003 Parent=0x0000000C SizeRef=1280,309 Split=X Selected=0xB159AA79\n\
-      DockNode    ID=0x00000009 Parent=0x00000003 SizeRef=988,470 CentralNode=1 Selected=0xB159AA79\n\
-      DockNode    ID=0x0000000A Parent=0x00000003 SizeRef=290,470 Selected=0xA488D3FA\n\
-    DockNode      ID=0x00000004 Parent=0x0000000C SizeRef=1280,229 Split=X Selected=0xDA8B5EC4\n\
-      DockNode    ID=0x00000005 Parent=0x00000004 SizeRef=502,220 Selected=0x2682A10E\n\
-      DockNode    ID=0x00000006 Parent=0x00000004 SizeRef=776,220 Split=X Selected=0xDA8B5EC4\n\
-        DockNode  ID=0x00000007 Parent=0x00000006 SizeRef=484,229 Selected=0xDA8B5EC4\n\
-        DockNode  ID=0x00000008 Parent=0x00000006 SizeRef=290,229 Selected=0x15C72E9A\n\
+DockSpace       ID=0x8B93E3BD Pos=0,19 Size=1280,701 Split=X Selected=0x7C3EDFF1\n\
+  DockNode      ID=0x00000001 Parent=0x8B93E3BD SizeRef=578,371 CentralNode=1 Selected=0x7C3EDFF1\n\
+  DockNode      ID=0x00000002 Parent=0x8B93E3BD SizeRef=275,371 HiddenTabBar=1 Selected=0x67284010\n\
+DockSpace       ID=0xD6C4A87D Window=0x1BBC0F80 Pos=0,19 Size=1280,701 Split=X Selected=0xB159AA79\n\
+  DockNode      ID=0x00000009 Parent=0xD6C4A87D SizeRef=988,470 Split=Y Selected=0xB159AA79\n\
+    DockNode    ID=0x00000005 Parent=0x00000009 SizeRef=988,470 Split=Y Selected=0xB159AA79\n\
+      DockNode  ID=0x0000000D Parent=0x00000005 SizeRef=988,158 Selected=0x376897DD\n\
+      DockNode  ID=0x0000000E Parent=0x00000005 SizeRef=988,310 CentralNode=1 Selected=0xB159AA79\n\
+    DockNode    ID=0x00000006 Parent=0x00000009 SizeRef=988,229 Split=X Selected=0x2682A10E\n\
+      DockNode  ID=0x00000007 Parent=0x00000006 SizeRef=494,229 Selected=0x2682A10E\n\
+      DockNode  ID=0x00000008 Parent=0x00000006 SizeRef=492,229 Selected=0xDA8B5EC4\n\
+  DockNode      ID=0x0000000A Parent=0xD6C4A87D SizeRef=290,470 Split=Y Selected=0xA488D3FA\n\
+    DockNode    ID=0x00000003 Parent=0x0000000A SizeRef=290,470 Split=Y Selected=0xA488D3FA\n\
+      DockNode  ID=0x0000000B Parent=0x00000003 SizeRef=290,158 Selected=0x839614CF\n\
+      DockNode  ID=0x0000000C Parent=0x00000003 SizeRef=290,310 Selected=0xA488D3FA\n\
+    DockNode    ID=0x00000004 Parent=0x0000000A SizeRef=290,229 Selected=0x15C72E9A\n\
 \n\
 \n\
 ";
